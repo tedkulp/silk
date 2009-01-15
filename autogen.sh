69b0bac74ab58255e0a4ea9fe0b2dcda78e5008f
secoif <tim@timoxley.com>
2009-01-15 20:11:12 +1000

    autogen.sh now prompts to backup setup.yml before overwriting new. Basic .htaccess added.

--- autogen.sh
+++ autogen.sh
@@ -1,5 +1,6 @@
 #!/bin/sh
 
+echo 'Setting up typical Silk Framework application directory structure and proper permissions...';
 mkdir -p tmp
 mkdir -p tmp/templates_c
 mkdir -p tmp/cache
@@ -21,6 +22,30 @@ mkdir -p log
 chmod -R 777 log
 
 cp lib/silk/index.php .
+cp lib/silk/.htaccess .
 
+
+# keep a single backup of old config if it happens to already exist
+if [ -f config/setup.yml ]; then
+	# check if the files differ
+	cmp -s lib/silk/config/setup.yml config/setup.yml;
+	if [ $? == 1 ]; then
+		echo -e "\nThe file config/setup.yml exists and has changed. Do you want to backup this file before replacing? Y/N"
+		read backup_setup;
+		case $backup_setup in
+		[yY]*)
+			mv config/setup.yml config/setup.yml.backup;
+			echo 'File config/setup.yml copied to setup.yml.backup'; 
+			cp lib/silk/config/setup.yml config/;
+			echo 'File default setup.yml written to config/setup.yml.';
+			echo 'You might want to "diff config/setup.yml config/setup.yml.backup" before reverting to your old setup.'; 
+			break;;
+		*)
+			cp lib/silk/config/setup.yml config/;
+			echo 'File config/setup.yml overwritten. Old setup.yml not backed up.';
+			break;;
+		esac
+	fi
+fi
 cp lib/silk/config/routes.php config/
-cp lib/silk/config/setup.yml config/
+echo -e "\nDONE";
