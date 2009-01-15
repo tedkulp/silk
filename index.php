693b745f006914e435483c6353a612693c953c9b
Ted Kulp <ted@cmsmadesimple.org>
2009-01-15 07:45:02 -0500

    Add ROOT_DIR define to index.php
    Also add a default if it's not set for some
    reason.  This allows us to possibly put the silk
    directory somewhere else on the server (like a
    PEAR installation).

--- index.php
+++ index.php
@@ -21,6 +21,8 @@
 // OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
 // THE SOFTWARE.
 
+define('ROOT_DIR', dirname(__FILE__));
+
 include_once('lib/silk/silk.api.php');
 
 SilkBootstrap::get_instance()->run();
