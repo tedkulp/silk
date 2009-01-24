56aefbb1790c7e13ff0d51b0ff69045867acdb3c
Ted Kulp <ted@cmsmadesimple.org>
2009-01-24 08:38:01 -0500

    Add missing }

--- classes/class.silk_request.php
+++ classes/class.silk_request.php
@@ -88,6 +88,7 @@ class SilkRequest extends SilkObject
 				}
 				echo $controller->run_action($params['action'], $params);
 			}
+		}
 		catch (SilkAccessException $ex)
 		{
 			die('access problem: ' . $ex);
