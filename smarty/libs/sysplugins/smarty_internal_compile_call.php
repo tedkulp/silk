617a7f973dc56bf3aa9f391b4c6a381bb0ab74bd
Ted Kulp <ted@cmsmadesimple.org>
2010-12-05 07:14:25 -0500

    Update Smarty to 3.0.5

--- smarty/libs/sysplugins/smarty_internal_compile_call.php
+++ smarty/libs/sysplugins/smarty_internal_compile_call.php
@@ -1,61 +1,106 @@
 <?php
 
 /**
-* Smarty Internal Plugin Compile Function_Call
-* 
-* Compiles the calls of user defined tags defined by {function}
-* 
-* @package Smarty
-* @subpackage Compiler
-* @author Uwe Tews 
-*/
+ * Smarty Internal Plugin Compile Function_Call
+ * 
+ * Compiles the calls of user defined tags defined by {function}
+ * 
+ * @package Smarty
+ * @subpackage Compiler
+ * @author Uwe Tews 
+ */
+
 /**
-* Smarty Internal Plugin Compile Function_Call Class
-*/
+ * Smarty Internal Plugin Compile Function_Call Class
+ */
 class Smarty_Internal_Compile_Call extends Smarty_Internal_CompileBase {
+	// attribute definitions
+    public $required_attributes = array('name');
+    public $shorttag_order = array('name');
+    public $optional_attributes = array('_any'); 
+
     /**
-    * Compiles the calls of user defined tags defined by {function}
-    * 
-    * @param array $args array with attributes from parser
-    * @param object $compiler compiler object
-    * @return string compiled code
-    */
+     * Compiles the calls of user defined tags defined by {function}
+     * 
+     * @param array $args array with attributes from parser
+     * @param object $compiler compiler object
+     * @param array $parameter array with compilation parameter
+     * @return string compiled code
+     */
     public function compile($args, $compiler)
     {
         $this->compiler = $compiler;
-        $this->required_attributes = array('name');
-        $this->optional_attributes = array('_any'); 
+        $this->smarty = $compiler->smarty;
         // check and get attributes
         $_attr = $this->_get_attributes($args); 
-        // save posible attributes
+        // save possible attributes
         if (isset($_attr['assign'])) {
             // output will be stored in a smarty variable instead of beind displayed
             $_assign = $_attr['assign'];
         } 
+        $_name = $_attr['name'];
+        unset($_attr['name'], $_attr['assign'], $_attr['nocache']); 
         // set flag (compiled code of {function} must be included in cache file
-        if ($this->compiler->nocache || $this->compiler->tag_nocache) {
-            $nocache = 'true';
+        if ($compiler->nocache || $compiler->tag_nocache) {
+            $_nocache = 'true';
         } else {
-            $nocache = 'false';
+            $_nocache = 'false';
         } 
-        // create template object
-        $_output = "<?php \$_template = new Smarty_Internal_Function_Call_Handler ({$_attr['name']}, \$_smarty_tpl->smarty, \$_smarty_tpl, {$nocache});\n"; 
-        // delete {include} standard attributes
-        unset($_attr['name'], $_attr['assign']); 
-        // remaining attributes must be assigned as smarty variable
-        if (!empty($_attr)) {
-            // create variables
-            foreach ($_attr as $_key => $_value) {
-                $_output .= "\$_template->assign('$_key',$_value);\n";
+        $_paramsArray = array();
+        foreach ($_attr as $_key => $_value) {
+            if (is_int($_key)) {
+                $_paramsArray[] = "$_key=>$_value";
+            } else {
+                $_paramsArray[] = "'$_key'=>$_value";
             } 
-        } 
+        }
+        if (isset($compiler->template->properties['function'][$_name]['parameter'])) {
+            foreach ($compiler->template->properties['function'][$_name]['parameter'] as $_key => $_value) {
+                if (!isset($_attr[$_key])) {
+                    if (is_int($_key)) {
+                        $_paramsArray[] = "$_key=>$_value";
+                    } else {
+                        $_paramsArray[] = "'$_key'=>$_value";
+                    } 
+                } 
+            } 
+        } elseif (isset($this->smarty->template_functions[$_name]['parameter'])) {
+           foreach ($this->smarty->template_functions[$_name]['parameter'] as $_key => $_value) {
+                if (!isset($_attr[$_key])) {
+                    if (is_int($_key)) {
+                        $_paramsArray[] = "$_key=>$_value";
+                    } else {
+                        $_paramsArray[] = "'$_key'=>$_value";
+                    } 
+                } 
+            } 
+        }
+        //varibale name?
+        if (!(strpos($_name,'$')===false)) {
+        	$call_cache = $_name;
+        	$call_function = '$tmp = "smarty_template_function_".'.$_name.'; $tmp';
+       	} else {
+        	$_name = trim($_name, "'\"");
+       		$call_cache = "'{$_name}'";
+         	$call_function = 'smarty_template_function_'.$_name;
+        }
+      	
+        $_params = 'array(' . implode(",", $_paramsArray) . ')';
+    	$_hash = str_replace('-','_',$compiler->template->properties['nocache_hash']);
         // was there an assign attribute
         if (isset($_assign)) {
-            $_output .= "\$_smarty_tpl->assign({$_assign},\$_template->getRenderedTemplate());\n";
+            if ($compiler->template->caching) {
+                $_output = "<?php ob_start(); Smarty_Internal_Function_Call_Handler::call ({$call_cache},\$_smarty_tpl,{$_params},'{$_hash}',{$_nocache}); \$_smarty_tpl->assign({$_assign}, ob_get_clean());?>\n";
+            } else {
+                $_output = "<?php ob_start(); {$call_function}(\$_smarty_tpl,{$_params}); \$_smarty_tpl->assign({$_assign}, ob_get_clean());?>\n";
+            } 
         } else {
-            $_output .= "echo \$_template->getRenderedTemplate();\n";
+            if ($compiler->template->caching) {
+                $_output = "<?php Smarty_Internal_Function_Call_Handler::call ({$call_cache},\$_smarty_tpl,{$_params},'{$_hash}',{$_nocache});?>\n";
+            } else {
+                $_output = "<?php {$call_function}(\$_smarty_tpl,{$_params});?>\n";
+            } 
         } 
-        $_output .= 'unset($_template);?>';
         return $_output;
     } 
 } 
--- smarty/libs/sysplugins/smarty_internal_compile_private_modifier.php
+++ smarty/libs/sysplugins/smarty_internal_compile_private_modifier.php
@@ -1,56 +1,66 @@
 <?php
 /**
-* Smarty Internal Plugin Compile Modifier
-* 
-* Compiles code for modifier execution
-* 
-* @package Smarty
-* @subpackage Compiler
-* @author Uwe Tews 
-*/
+ * Smarty Internal Plugin Compile Modifier
+ * 
+ * Compiles code for modifier execution
+ * 
+ * @package Smarty
+ * @subpackage Compiler
+ * @author Uwe Tews 
+ */
+
 /**
-* Smarty Internal Plugin Compile Modifier Class
-*/
+ * Smarty Internal Plugin Compile Modifier Class
+ */
 class Smarty_Internal_Compile_Private_Modifier extends Smarty_Internal_CompileBase {
     /**
-    * Compiles code for modifier execution
-    * 
-    * @param array $args array with attributes from parser
-    * @param object $compiler compiler object
-    * @return string compiled code
-    */
-    public function compile($args, $compiler)
+     * Compiles code for modifier execution
+     * 
+     * @param array $args array with attributes from parser
+     * @param object $compiler compiler object
+     * @param array $parameter array with compilation parameter
+     * @return string compiled code
+     */
+    public function compile($args, $compiler, $parameter)
     {
         $this->compiler = $compiler;
         $this->smarty = $this->compiler->smarty;
-        $this->required_attributes = array('modifier', 'params'); 
         // check and get attributes
-        $_attr = $this->_get_attributes($args); 
-        // check for registered modifier
-        if (isset($compiler->smarty->registered_plugins['modifier'][$_attr['modifier']])) {
-            $function = $compiler->smarty->registered_plugins['modifier'][$_attr['modifier']][0];
-            if (!is_array($function)) {
-                $output = "{$function}({$_attr['params']})";
-            } else if (is_object($function[0])) {
-                $output = 'call_user_func_array($_smarty_tpl->smarty->registered_plugins[\'modifier\'][\'' . $_attr['modifier'] . '\'][0],array(' . $_attr['params'] . '))';
+        $_attr = $this->_get_attributes($args);
+        $output = $parameter['value']; 
+        // loop over list of modifiers
+        foreach ($parameter['modifierlist'] as $single_modifier) {
+            $modifier = $single_modifier[0];
+	   $single_modifier[0] = $output;
+            $params = implode(',', $single_modifier); 
+            // check for registered modifier
+            if (isset($compiler->smarty->registered_plugins[Smarty::PLUGIN_MODIFIER][$modifier])) {
+                $function = $compiler->smarty->registered_plugins[Smarty::PLUGIN_MODIFIER][$modifier][0];
+                if (!is_array($function)) {
+                    $output = "{$function}({$params})";
+                } else {
+                    if (is_object($function[0])) {
+                        $output = '$_smarty_tpl->smarty->registered_plugins[Smarty::PLUGIN_MODIFIER][\'' . $modifier . '\'][0][0]->' . $function[1] . '(' . $params . ')';
+                    } else {
+                        $output = $function[0] . '::' . $function[1] . '(' . $params . ')';
+                    } 
+                } 
+                // check for plugin modifiercompiler
+            } else if ($compiler->smarty->loadPlugin('smarty_modifiercompiler_' . $modifier)) {
+                $plugin = 'smarty_modifiercompiler_' . $modifier;
+                $output = $plugin($single_modifier, $compiler); 
+                // check for plugin modifier
+            } else if ($function = $this->compiler->getPlugin($modifier, Smarty::PLUGIN_MODIFIER)) {
+                $output = "{$function}({$params})"; 
+                // check if trusted PHP function
+            } else if (is_callable($modifier)) {
+                // check if modifier allowed
+                if (!is_object($this->smarty->security_policy) || $this->smarty->security_policy->isTrustedModifier($modifier, $this->compiler)) {
+                    $output = "{$modifier}({$params})";
+                } 
             } else {
-                $output = 'call_user_func_array(array(\'' . $function[0] . '\',\'' . $function[1] . '\'),array(' . $_attr['params'] . '))';
+                $this->compiler->trigger_template_error ("unknown modifier \"" . $modifier . "\"", $this->compiler->lex->taglineno);
             } 
-        // check for plugin modifier
-        } else if ($function = $this->compiler->getPlugin($_attr['modifier'], 'modifier')) {
-            if (!is_array($function)) {
-                $output = "{$function}({$_attr['params']})";
-            } else {
-                $output = 'call_user_func_array(array(\'' . $function[0] . '\',\'' . $function[1] . '\'),array(' . $_attr['params'] . '))';
-            } 
-            // check if trusted PHP function
-        } else if (is_callable($_attr['modifier'])) {
-            // check if modifier allowed
-            if (!$this->compiler->template->security || $this->smarty->security_handler->isTrustedModifier($_attr['modifier'], $this->compiler)) {
-                $output = "{$_attr['modifier']}({$_attr['params']})";
-            } 
-        } else {
-            $this->compiler->trigger_template_error ("unknown modifier \"" . $_attr['modifier'] . "\"");
         } 
         return $output;
     } 
