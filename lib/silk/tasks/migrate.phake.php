472cedac3dd50edf3216a173f60bb9162921da94
Ted Kulp <ted@cmsmadesimple.org>
2011-03-14 23:24:20 -0400

    Added phake task to migrate models.
    Also added in an "orig" config.php file for use
    for setting up a project for now. Once project
    setup is working, we can remove it and use a
    generated one instead.

---
+++ lib/silk/tasks/migrate.phake.php
@@ -0,0 +1,78 @@
+<?php // -*- mode:php; tab-width:4; indent-tabs-mode:t; c-basic-offset:4; -*-
+// The MIT License
+// 
+// Copyright (c) 2008-2011 Ted Kulp
+// 
+// Permission is hereby granted, free of charge, to any person obtaining a copy
+// of this software and associated documentation files (the "Software"), to deal
+// in the Software without restriction, including without limitation the rights
+// to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+// copies of the Software, and to permit persons to whom the Software is
+// furnished to do so, subject to the following conditions:
+// 
+// The above copyright notice and this permission notice shall be included in
+// all copies or substantial portions of the Software.
+// 
+// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+// IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+// FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+// AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+// LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+// OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+// THE SOFTWARE.
+
+desc('Run migrations automatically for models');
+task('migrate', function($app)
+{
+	$config = get('config');
+	$models = array();
+	if (isset($config['auto_migrate']))
+	{
+		if (isset($config['auto_migrate']['include']) && is_array($config['auto_migrate']['include']))
+		{
+			foreach($config['auto_migrate']['include'] as $one_entry)
+			{
+				$models = array_merge($models, glob(ROOT_DIR . '/components/*/models/' . $one_entry . '.php'));
+			}
+		}
+		$models = array_unique($models);
+		if (isset($config['auto_migrate']['exclude']) && is_array($config['auto_migrate']['exclude']))
+		{
+			$exclude = array();
+			foreach($config['auto_migrate']['exclude'] as $one_entry)
+			{
+				foreach($models as $one_model)
+				{
+					if (strpos($one_model, $one_entry) !== false)
+					{
+						$exclude[] = $one_model;
+						break;
+					}
+				}
+			}
+			$exclude = array_unique($exclude);
+			$models = array_diff($models, $exclude);
+		}
+	}
+
+	if (count($models))
+	{
+		foreach ($models as $one_model)
+		{
+			$model = basename($one_model, '.' . substr(strrchr($one_model, '.'), 1));
+			if (class_exists($model) && is_subclass_of($model, '\silk\model\Model'))
+			{
+				echo "Migrating model: " . $model . "\n";
+				$model::migrate();
+			}
+		}
+		echo "Done!\n";
+	}
+	else
+	{
+		echo "No models found\n";
+	}
+
+});
+
+# vim:ts=4 sw=4 noet filetype=php
